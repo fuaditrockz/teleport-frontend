f66d7ca31c4b7cf11fb9c4c08ec65ed8e8d65015
Muhammad Fuadit <muhammadfuaditrockz@gmail.com>
2017-07-09 19:05:07 +0700

    Delete file backup for teleport core.

--- teleport-core/styles/core.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  'logo-33planet': {
-    'marginTop': [{ 'unit': 'px', 'value': 10 }],
-    'width': [{ 'unit': '%H', 'value': 0.15 }],
-    'height': [{ 'unit': '%V', 'value': 0.15 }]
-  }
-});
